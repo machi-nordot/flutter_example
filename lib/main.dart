5a67afe23e3b540cedba642fc5b2653d426223ec
Yoshiaki Machi <machi@MacBook-Pro.local>
2019-11-30 21:05:10 +0900

    bible-3-4-1-asset-load-image

--- lib/main.dart
+++ lib/main.dart
@@ -10,20 +10,12 @@ class MyApp extends StatelessWidget {
       appBar: AppBar(
         title: Text('Asset demo'),
       ),
-      body: StringAsset(),
+      body: ImageAsset(),
     )
   );
 }
 
-class StringAsset extends StatelessWidget {
+class ImageAsset extends StatelessWidget {
   @override
-  Widget build(BuildContext context) => Center(
-    child: Container(
-      // Futureの結果をウィジットにマッピングする際は、FutureBuilderを使用する。
-      child: FutureBuilder(
-        future: DefaultAssetBundle.of(context).loadString('assets/top_text.txt'),
-        builder: (context, snapshot) => Text(snapshot.data),
-      ),
-    ),
-  );
+  Widget build(BuildContext context) => Image.asset('assets/image.png');
 }
