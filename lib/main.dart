031ea414afa07a8b4787fbb94d47909fe037ef1b
Yoshiaki Machi <machi@MacBook-Pro.local>
2019-11-30 18:20:41 +0900

    bible-3-2-2-widget-layout-container

--- lib/main.dart
+++ lib/main.dart
@@ -4,101 +4,48 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    final stars = Row(
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        Icon(Icons.star, color: Colors.green[500]),
-        Icon(Icons.star, color: Colors.green[500]),
-        Icon(Icons.star, color: Colors.green[500]),
-        Icon(Icons.star, color: Colors.black),
-        Icon(Icons.star, color: Colors.black),
-      ],
-    );
-
-    final ratings = Container(
-      padding: EdgeInsets.all(20),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          stars,
-          Text(
-            '170 Reviews',
-            style: TextStyle(
-              color: Colors.black,
-              fontWeight: FontWeight.w800,
-              fontFamily: 'Roboto',
-              letterSpacing: 0.5,
-              fontSize: 20,
-            )
-          )
-        ],
-      )
-    );
-
-    final descTextStyle = TextStyle(
-      color: Colors.black,
-      fontWeight: FontWeight.w800,
-      fontFamily: 'Roboto',
-      letterSpacing: 0.5,
-      fontSize: 18,
-      height: 2,
-    );
-
-    // DefaultTextStyle.merge()を使用することで、styleに指定した要素を
-    // child以下にあるテキスト要素に反映することができる。
-    final iconList = DefaultTextStyle.merge(
-      style: descTextStyle,
-      child: Container(
-        padding: EdgeInsets.all(20),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Column(
-              children: <Widget>[
-                Icon(Icons.kitchen, color: Colors.green[500]),
-                Text('PREP:'),
-                Text('25 min')
-              ],
-            ),
-            Column(
-              children: <Widget>[
-                Icon(Icons.timer, color: Colors.green[500]),
-                Text('COOK:'),
-                Text('1 hr')
-              ],
-            ),
-            Column(
-              children: <Widget>[
-                Icon(Icons.restaurant, color: Colors.green[500]),
-                Text('FEEDS:'),
-                Text('4-6')
-              ],
-            ),
-          ],
-        )
-      )
-    );
-
-    final sample = Container(
-      padding: EdgeInsets.fromLTRB(20, 30, 20, 20),
-      child: Column(
-        children: <Widget>[
-          Text('titleText'),
-          Text('subTitle'),
-          ratings,
-          iconList,
-        ],
+  Widget build(BuildContext context) => MaterialApp(
+    title: 'Widgets layout demo',
+    theme: ThemeData(primarySwatch: Colors.blue),
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Widgets layout demo'),
       ),
-    );
+      body: _buildImageContainer([
+        'images/pic1.jpg',
+        'images/pic2.jpg',
+        'images/pic3.jpg',
+        'images/pic1.jpg',
+        'images/pic2.jpg',
+        'images/pic3.jpg',
+      ]),
+    ),
+  );
 
-    return MaterialApp(
-      title: 'Flutter layout demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter layout demo'),
-        ),
-        body: sample,
+  Widget _buildImageContainer(List<String> images) {
+    final slicedImages = images.fold<List<List<String>>>([], (list, image) {
+      if (list.length == 0 || list.last.length >= 2) {
+        list.add(<String>[]);
+      }
+      list.last.add(image);
+      return list;
+    });
+
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+      ),
+      child: Column(
+        children: slicedImages.map((rowImages) => Row(
+          children: rowImages.map((image) => Container(
+            decoration: BoxDecoration(
+              border: Border.all(width: 10, color: Colors.black38),
+              borderRadius: BorderRadius.all(Radius.circular(8)),
+            ),
+            margin: EdgeInsets.all(4),
+            child: Image.asset(image, width: 150),
+          )).toList(),
+        )).toList(),
       ),
     );
   }
