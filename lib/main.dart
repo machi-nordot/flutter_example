1afd4ffb2116bfd92037f8ea8ad3d6e5b6b97b64
Yoshiaki Machi <machi@MacBook-Pro.local>
2019-11-30 17:10:31 +0900

    bible-3-1-3-stateful-widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,14 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _counter = 0;
+
   @override
   Widget build(BuildContext context) => MaterialApp(
     title: 'Flutter Demo',
@@ -11,11 +18,30 @@ class MyApp extends StatelessWidget {
     ),
     home: Scaffold(
       body: Center(
-        child: Text(
-          'Flutter Demo Home Page',
-          style: Theme.of(context).textTheme.display1,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
+            )
+          ],
         )
-      )
+      ),
+      floatingActionButton: FloatingActionButton(
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+        onPressed: _incrementCounter,
+      ),
     )
   );
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
 }
