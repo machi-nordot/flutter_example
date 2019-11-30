35aa97d84b56475885f8816cca362fedd16e38a9
Yoshiaki Machi <machi@MacBook-Pro.local>
2019-11-30 17:22:19 +0900

    bible-3-2-1-basic-layout-non-material-design-2

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,17 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) => Center(
-    child: Text('Hello World', textDirection: TextDirection.ltr),
+  Widget build(BuildContext context) => Container(
+    decoration: BoxDecoration(color: Colors.white),
+    child: Center(
+      child: Text(
+        'Hello World',
+        textDirection: TextDirection.ltr,
+        style: TextStyle(
+          fontSize: 32,
+          color: Colors.black87,
+        )
+      )
+    )
   );
 }
