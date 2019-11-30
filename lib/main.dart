14a698defa4f62482983a68e497b04af340ec77f
Yoshiaki Machi <machi@MacBook-Pro.local>
2019-11-30 20:30:15 +0900

    bible-3-4-1-asset-load-string

--- lib/main.dart
+++ lib/main.dart
@@ -5,105 +5,25 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) => MaterialApp(
-    title: 'Navigation',
+    title: 'Asset demo',
     home: Scaffold(
       appBar: AppBar(
-        title: Text('TapBox'),
+        title: Text('Asset demo'),
       ),
-      body: Center(
-        child: ParentWidget(),
-      )
+      body: StringAsset(),
     )
   );
 }
 
-class ParentWidget extends StatefulWidget {
+class StringAsset extends StatelessWidget {
   @override
-  _ParentWidgetState createState() => _ParentWidgetState();
-}
-
-class _ParentWidgetState extends State<ParentWidget> {
-  bool _active = false;
-
-  @override
-  Widget build(BuildContext context) => Container(
-    child: TapBoxC(
-      active: _active,
-      onChanged: _handleTapBoxChanged,
-    ),
-  );
-
-  void _handleTapBoxChanged(bool newValue) {
-    setState(() {
-      _active = newValue;
-    });
-  }
-}
-
-class TapBoxC extends StatefulWidget {
-  TapBoxC({ Key key, this.active: false, @required this.onChanged })
-    : assert(active != null),
-      assert(onChanged != null),
-      super(key: key);
-  
-  final bool active;
-  final ValueChanged<bool> onChanged;
-
-  @override
-  _TapBoxCState createState() => _TapBoxCState();
-}
-
-class _TapBoxCState extends State<TapBoxC> {
-  bool _highlight = false;
-
-  @override
-  Widget build(BuildContext context) => GestureDetector(
+  Widget build(BuildContext context) => Center(
     child: Container(
-      child: Center(
-        child: Text(
-          // 変数widgetでTapBoxCを参照できる
-          widget.active ? 'Active' : 'Inactive',
-          style: TextStyle(fontSize: 32.0, color: Colors.white)
-        ),
-      ),
-      width: 200.0,
-      height: 200.0,
-      decoration: BoxDecoration(
-        // 変数widgetでTapBoxCを参照できる
-        color: widget.active ? Colors.lightGreen[700] : Colors.grey[600],
-        // 要素をタップしている間、枠線にハイライトが表示される
-        border: _highlight ? Border.all(
-          color: Colors.teal[700],
-          width: 10.0,
-        ) : null,
+      // Futureの結果をウィジットにマッピングする際は、FutureBuilderを使用する。
+      child: FutureBuilder(
+        future: DefaultAssetBundle.of(context).loadString('assets/top_text.txt'),
+        builder: (context, snapshot) => Text(snapshot.data),
       ),
     ),
-    onTapDown: _handleTapDown,
-    onTapUp: _handleTapUp,
-    onTapCancel: _handleTapCancel,
-    onTap: _handleTap,
   );
-
-  void _handleTapDown(TapDownDetails details) {
-    setState(() {
-      _highlight = true;
-    });
-  }
-
-  void _handleTapUp(TapUpDetails details) {
-    setState(() {
-      _highlight = false;
-    });
-  }
-
-  void _handleTapCancel() {
-    setState(() {
-      _highlight = false;
-    });
-  }
-
-  void _handleTap() {
-    // 変数widgetでTapBoxCを参照できる
-    widget.onChanged(!widget.active);
-  }
 }
