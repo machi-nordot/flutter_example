717416ca6f85a7efc9ddf7be29928786a70f966b
Yoshiaki Machi <machi@MacBook-Pro.local>
2019-11-30 17:38:58 +0900

    bible-3-2-1-basic-layout-row-size-over

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,12 @@ class MyApp extends StatelessWidget {
       appBar: AppBar(
         title: Text('Flutter layout demo'),
       ),
-      body: Column(
+      body: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget>[
+          Image.asset('images/pic1.jpg'),
+          Image.asset('images/pic2.jpg'),
+          Image.asset('images/pic3.jpg'),
           Image.asset('images/pic1.jpg'),
           Image.asset('images/pic2.jpg'),
           Image.asset('images/pic3.jpg'),
