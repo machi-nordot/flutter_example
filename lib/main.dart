9b5097a403a443779ad5dd637cb7f8a04918a000
Yoshiaki Machi <machi@MacBook-Pro.local>
2019-11-30 20:03:41 +0900

    bible-3-3-2-state-parent-and-own

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class _ParentWidgetState extends State<ParentWidget> {
 
   @override
   Widget build(BuildContext context) => Container(
-    child: TapBoxB(
+    child: TapBoxC(
       active: _active,
       onChanged: _handleTapBoxChanged,
     ),
@@ -40,8 +40,8 @@ class _ParentWidgetState extends State<ParentWidget> {
   }
 }
 
-class TapBoxB extends StatelessWidget {
-  TapBoxB({ Key key, this.active: false, @required this.onChanged })
+class TapBoxC extends StatefulWidget {
+  TapBoxC({ Key key, this.active: false, @required this.onChanged })
     : assert(active != null),
       assert(onChanged != null),
       super(key: key);
@@ -50,24 +50,60 @@ class TapBoxB extends StatelessWidget {
   final ValueChanged<bool> onChanged;
 
   @override
+  _TapBoxCState createState() => _TapBoxCState();
+}
+
+class _TapBoxCState extends State<TapBoxC> {
+  bool _highlight = false;
+
+  @override
   Widget build(BuildContext context) => GestureDetector(
     child: Container(
       child: Center(
         child: Text(
-          active ? 'Active' : 'Inactive',
+          // 変数widgetでTapBoxCを参照できる
+          widget.active ? 'Active' : 'Inactive',
           style: TextStyle(fontSize: 32.0, color: Colors.white)
         ),
       ),
       width: 200.0,
       height: 200.0,
       decoration: BoxDecoration(
-        color: active ? Colors.lightGreen[700] : Colors.grey[600],
+        // 変数widgetでTapBoxCを参照できる
+        color: widget.active ? Colors.lightGreen[700] : Colors.grey[600],
+        // 要素をタップしている間、枠線にハイライトが表示される
+        border: _highlight ? Border.all(
+          color: Colors.teal[700],
+          width: 10.0,
+        ) : null,
       ),
     ),
+    onTapDown: _handleTapDown,
+    onTapUp: _handleTapUp,
+    onTapCancel: _handleTapCancel,
     onTap: _handleTap,
   );
 
+  void _handleTapDown(TapDownDetails details) {
+    setState(() {
+      _highlight = true;
+    });
+  }
+
+  void _handleTapUp(TapUpDetails details) {
+    setState(() {
+      _highlight = false;
+    });
+  }
+
+  void _handleTapCancel() {
+    setState(() {
+      _highlight = false;
+    });
+  }
+
   void _handleTap() {
-    onChanged(!active);
+    // 変数widgetでTapBoxCを参照できる
+    widget.onChanged(!widget.active);
   }
 }
